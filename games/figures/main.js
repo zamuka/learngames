6a9bbd6fdb171fa00be0b3718bc9a69b97deb69b
serhii.zamoshnikov <sergey.zamoshnikov@dev-pro.net>
2019-04-28 11:30:23 +0300

    adds line function implmentation

--- games/figures/main.js
+++ games/figures/main.js
@@ -51,7 +51,66 @@ function initAxes() {
 }
 
 function line(x1, y1, x2, y2) {
+  const dx = x2 - x1;
+  const dy = y2 - y1;
 
+  const lengthX = Math.abs(dx);
+  const lengthY = Math.abs(dy);
+
+  let pixelCount = lengthX;
+
+  if (lengthX < lengthY) {
+    pixelCount = lengthY;
+  }
+
+  const stepX = dx / pixelCount;
+  const stepY = dy / pixelCount;
+
+  for (let i = 0; i <= pixelCount; i = i + 1) {
+    const pointX = x1 + stepX * i;
+    const pointY = y1 + stepY * i;
+
+    setPixel(pointX, pointY);
+  }
+}
+
+function drawPapaPixels(i) {
+  setPixel(50, 50 + i);
+  setPixel(150, 50 + i);
+  setPixel(50 + i, 50);
+
+  setPixel(200 + i / 2, 150 - i);
+  setPixel(250 + i / 2, 50 + i);
+  setPixel(225 + i / 2, 100);
+
+  setPixel(350, 50 + i);
+  setPixel(450, 50 + i);
+  setPixel(350 + i, 50);
+
+  setPixel(500 + i / 2, 150 - i);
+  setPixel(550 + i / 2, 50 + i);
+  setPixel(525 + i / 2, 100);
+}
+
+function drawRomaPixels(i) {
+  setPixel(50, 200 + i);
+  setPixel(50 + i / 2, 200);
+  setPixel(100, 200 + i / 2);
+  setPixel(50 + i / 2, 250);
+
+  setPixel(150, 200 + i);
+  setPixel(150 + i / 2, 200);
+  setPixel(200, 200 + i);
+  setPixel(150 + i / 2, 300);
+
+  setPixel(250, 200 + i);
+  setPixel(250 + i / 2, 200 + i);
+  setPixel(350 - i / 2, 200 + i);
+  setPixel(350, 200 + i);
+
+  setPixel(400 + i / 2, 300 - i);
+  setPixel(450 + i / 2, 200 + i);
+  setPixel(425 + i / 2, 250);
 }
 
 function main() {
@@ -59,42 +118,9 @@ function main() {
 
   for (let i = 0; i < 100; i = i + 1) {
     setTimeout(() => {
-      setPixel(50, 50 + i);
-      setPixel(150, 50 + i);
-      setPixel(50 + i, 50);
-
-      setPixel(200 + i / 2, 150 - i);
-      setPixel(250 + i / 2, 50 + i);
-      setPixel(225 + i / 2, 100);
-
-      setPixel(350, 50 + i);
-      setPixel(450, 50 + i);
-      setPixel(350 + i, 50);
-
-      setPixel(500 + i / 2, 150 - i);
-      setPixel(550 + i / 2, 50 + i);
-      setPixel(525 + i / 2, 100);
-
-
-      setPixel(50, 200 + i);
-      setPixel(50 + i / 2, 200);
-      setPixel(100, 200 + i / 2);
-      setPixel(50 + i / 2, 250);
-
-      setPixel(150, 200 + i);
-      setPixel(150 + i / 2, 200);
-      setPixel(200, 200 + i);
-      setPixel(150 + i / 2, 300);
-
-      setPixel(250, 200 + i);
-      setPixel(250 + i / 2, 200 + i);
-      setPixel(350 - i / 2, 200 + i);
-      setPixel(350, 200 + i);
-
-      setPixel(400 + i / 2, 300 - i);
-      setPixel(450 + i / 2, 200 + i);
-      setPixel(425 + i / 2, 250);
-
+      drawPapaPixels(i);
+      drawRomaPixels(i);
+      // Word misha
       setPixel(50, 450 + i);
       setPixel(50 + i / 2, 450 + i);
       setPixel(100 + i / 2, 550 - i);
@@ -112,9 +138,24 @@ function main() {
       setPixel(500 + i / 2, 550 - i);
       setPixel(550 + i / 2, 450 + i);
       setPixel(525 + i / 2, 500);
+
+      const r = 12;
+      const cx = 300;
+      const cy = 300;
+
+      const a = 2 * Math.PI * i / 100;
+
+      const x = r * 2 * Math.cos(a);
+      const y = r * 2 * Math.sin(a);
+
+      setPixel(cx + x, cy + y);
     }, 20 * i);
   }
-  // line(0, 0, 200, -100);
+  // line(0, 0, 300, 300);
+  // line(80, 500, 300, 300);
+  for (let j = 0; j < 20; j = j + 1) {
+    line(300, 300, Math.random() * 600, Math.random() * 600);
+  }
 }
 
 window.onload = main;
