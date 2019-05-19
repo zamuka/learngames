bff0b4d48be4669c9781a6a8ed991157243d44d9
RomanZam <romazam04@gmail.com>
2019-05-19 20:36:42 +0300

    four point star added

--- games/sandbox/roma/main.js
+++ games/sandbox/roma/main.js
@@ -3,10 +3,15 @@ import { init, line } from '../canvasLibrary.js';
 function main() {
   init();
   for (let i = 0; i <= 500; i = i + 25) {
-    line(0, i, i, 0);
-    line(500 - i, 500, 500, 500 - i);
-    line(500 - i, 500, 0, i);
-    line(500 - i, 0, 500, i);
+    // line(0, i, i, 0);
+    // line(500 - i, 500, 500, 500 - i);
+    // line(500 - i, 500, 0, i);
+    // line(500 - i, 0, 500, i);
+    // line(0, i, i, 0);
+    line(0 + i, 500, 500, 500 - i);
+    line(500 + i, 500, 500, i);
+    line(500 - i, 500, 500, 1000 - i);
+    line(500 + i, 500, 500, 1000 - i);
   }
 }
 
