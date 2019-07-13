abac82ef7d35108ce0038853d185b4e6448338e9
RomanZam <romazam04@gmail.com>
2019-07-13 11:47:42 +0300

    stars above terrain and new sky color added

--- games/moonlander/main.js
+++ games/moonlander/main.js
@@ -3,12 +3,13 @@ import {
 } from '../sandbox/canvasLibrary.js';
 
 const heightMap = [];
+const ZOOM = 2;
 
 function clearScreen(color) {
   ctx.save();
 
   ctx.fillStyle = color;
-  ctx.fillRect(0, 0, canvas.width - 1, canvas.height - 1);
+  ctx.fillRect(0, 0, (canvas.width / ZOOM) - 1, (canvas.height / ZOOM) - 1);
 
   ctx.restore();
 }
@@ -19,7 +20,7 @@ function drawLandscape(height) {
   let vy = 0;
   let ay = 0;
 
-  while (x < canvas.width) {
+  while (x < canvas.width / ZOOM) {
     setPixel(x, y);
     heightMap.push(Math.floor(y));
 
@@ -32,16 +33,32 @@ function drawLandscape(height) {
   }
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function addStar() {
+  const x = getRandomInt(canvas.width / ZOOM);
+  const y = getRandomInt(canvas.height / ZOOM);
+  if (y < heightMap[x]) { setPixel(x, y); }
+  console.log(x);
+}
+
 function main() {
-  init(2);
+  init(ZOOM);
   ctx.fillStyle = 'rgb(255, 255, 255)';
 
-  clearScreen('rgb(0, 0, 0)');
+  clearScreen('rgb( 54, 49, 137)');
 
   drawLandscape(200);
 
   console.log(heightMap);
   console.log(heightMap[200]);
+
+  for (let i = 0; i < 50; i = i + 1) {
+    addStar();
+  }
 }
+
 
 window.onload = main;
