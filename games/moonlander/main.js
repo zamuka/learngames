49401deb1f88d885bad887dbf113e7353c341034
zamukan@gmail.com <zamukan@gmail.com>
2019-08-05 11:36:16 +0300

    separates creating and drawings. adds a ship. Trees removed.

--- games/moonlander/main.js
+++ games/moonlander/main.js
@@ -2,20 +2,37 @@ import {
   init, ctx, canvas, setPixel, line,
 } from '../sandbox/canvasLibrary.js';
 
-const heightMap = [];
 const ZOOM = 2;
 const padMinBorderDistance = 50;
-const TREE_HEIGHT = 25;
-let starCount = 0;
-let snowCount = 0;
+// const TREE_HEIGHT = 25;
+let frameTimeStamp = 0;
 
-const landingPad = {
-  width: 40,
-  position: -1,
-  start: -1,
-  end: -1,
+
+const level = {
+  g: 5,
+  snowSpawnHeight: 160,
+  stars: [],
+  heightMap: [],
+  pad: {
+    width: 40,
+    position: -1,
+    start: -1,
+    end: -1,
+  },
+  lander: {
+    x: 40,
+    y: 40,
+    /**
+     * axis x speed in pixels per second
+     */
+    vx: 20,
+    vy: 0,
+    image: new Image(),
+    width: 32,
+    height: 32,
+  },
 };
-const distanceFromPadForTrees = 10;
+
 function getScreenWidth() {
   return canvas.width / ZOOM;
 }
@@ -33,24 +50,22 @@ function clearScreen(color) {
   ctx.restore();
 }
 
-function drawLandscape(height) {
+function createLandscape(height) {
   let x = 0;
   let y = height;
   let vy = 0;
   let ay = 0;
-
-  landingPad.position = Math.round(Math.random() * (getScreenWidth() - padMinBorderDistance * 2))
+  const MAX_SLOPE = 2;
+  level.pad.position = Math.round(Math.random() * (getScreenWidth() - padMinBorderDistance * 2))
     + padMinBorderDistance;
 
-  const MAX_SLOPE = 2;
-  landingPad.start = landingPad.position - landingPad.width / 2;
-  landingPad.end = landingPad.position + landingPad.width / 2;
+  level.pad.start = level.pad.position - level.pad.width / 2;
+  level.pad.end = level.pad.position + level.pad.width / 2;
 
   while (x < getScreenWidth()) {
-    setPixel(x, y);
-    heightMap.push(Math.floor(y));
+    level.heightMap.push(Math.floor(y));
     x = x + 1;
-    const isOnPad = x > landingPad.start && x < landingPad.end;
+    const isOnPad = x > level.pad.start && x < level.pad.end;
     if (!isOnPad) {
       ay = Math.random() - 0.5 + 0.001 * (height - y);
       vy = vy + ay;
@@ -62,15 +77,17 @@ function drawLandscape(height) {
   }
 }
 
-function addTrees(treeCount) {
-  for (let i = 0; i < treeCount; i = i + 1) {
-    const treeX = getRandomInt(getScreenWidth());
-    if (treeX < landingPad.start - distanceFromPadForTrees
-        || treeX > landingPad.end + distanceFromPadForTrees) {
-      line(treeX, heightMap[treeX] - TREE_HEIGHT, treeX, heightMap[treeX]);
+function drawLandscape() {
+  for (let x = 0; x < level.heightMap.length; x = x + 1) {
+    const y = level.heightMap[x];
+    setPixel(x, y);
+    setPixel(x, y + 1);
+    if (y < level.snowSpawnHeight) {
+      ctx.save();
+      ctx.fillStyle = 'rgb(255, 255, 255)';
+      setPixel(x, y - 1);
+      ctx.restore();
     }
-    console.log('treeX :', treeX);
-    console.log('landingPad :', landingPad.position);
   }
 }
 
@@ -84,61 +101,77 @@ function getRandomInt(max) {
 }
 
 function addStar() {
-  let starX;
-  let starY;
-
+  const star = { x: 0, y: 0 };
   do {
-    starX = getRandomInt(getScreenWidth());
-    starY = getRandomInt(getScreenHeight());
-  } while (starY > heightMap[starX]);
+    star.x = getRandomInt(getScreenWidth());
+    star.y = getRandomInt(getScreenHeight());
+  } while (star.y > level.heightMap[star.x]);
 
-  setPixel(starX, starY);
-  starCount = starCount + 1;
+  level.stars.push(star);
 }
 
-const drawStars = () => {
+function drawStars() {
   ctx.save();
   ctx.fillStyle = 'rgb(255, 255, 255)';
-  for (let i = 0; i < 100; i = i + 1) {
-    addStar();
+
+  for (let i = 0; i < level.stars.length; i = i + 1) {
+    const star = level.stars[i];
+    setPixel(star.x, star.y);
   }
   ctx.restore();
-};
+}
 
-function main() {
-  init(ZOOM);
+function createStars(starCount) {
+  while (level.stars.length < starCount) {
+    addStar();
+  }
+}
+
+function moveLander(frameTime) {
+  const lander = level.lander;
+  lander.vy = lander.vy + level.g * frameTime;
+  lander.x = lander.x + lander.vx * frameTime;
+  lander.y = lander.y + lander.vy * frameTime;
+}
+
+function drawLander() {
+  const lander = level.lander;
+  const x = lander.x - Math.round(lander.width / 2);
+  const y = lander.y - Math.round(lander.height / 2);
+  ctx.drawImage(lander.image, x, y, 16, 16);
+}
+
+function drawFrame(dt) {
   ctx.fillStyle = 'rgb(150, 150, 150)';
 
   clearScreen('rgb( 54, 49, 137)');
 
-  drawLandscape(200);
 
-  const drawPixelBelowGround = (x) => {
-    setPixel(x, heightMap[x] + 1);
-  };
-
-  let j = 0;
-  while (j < heightMap.length) {
-    drawPixelBelowGround(j);
-    j = j + 1;
-  }
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  const snowSpawnHeight = 160;
-  for (let k = 0; k < heightMap.length; k = k + 1) {
-    const y = heightMap[k] - 1;
-    if (y < snowSpawnHeight) {
-      setPixel(k, y);
-      snowCount = snowCount + 1;
-    }
-  }
-
+  drawLandscape();
   drawStars();
-  console.log('starCount :', starCount);
-  console.log('snowCount :', snowCount);
 
-  console.log(landingPad);
+  drawLander();
+}
 
-  addTrees(10);
+function nextFrame(timestamp) {
+  // time passed from previous frame in seconds
+  const dt = (timestamp - frameTimeStamp) / 1000;
+  frameTimeStamp = timestamp;
+
+  moveLander(dt);
+
+  drawFrame(dt);
+  requestAnimationFrame(nextFrame);
+}
+
+function main() {
+  init(ZOOM);
+
+  createLandscape(200);
+  createStars(100);
+  level.lander.image.src = 'spaceship-32x32.png';
+
+  requestAnimationFrame(nextFrame);
 }
 
 
